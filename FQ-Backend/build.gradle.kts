1cab0bed16080629340583947d3a5a634d41eede
Beau <mail@beauver.com>
2025-10-02 13:35:46 +0200

    :sparkles: config

--- FQ-Backend/build.gradle.kts
+++ FQ-Backend/build.gradle.kts
@@ -13,6 +13,10 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation("io.quarkus:quarkus-jdbc-postgresql")
+    implementation("io.quarkus:quarkus-hibernate-orm-panache")
+    implementation("com.google.code.gson:gson:2.13.2")
+
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-arc")
     implementation("io.quarkus:quarkus-rest")
